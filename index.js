28b16530c55cde77cc5adfa000c371e2a6967e53
Mohammed Zakaria Alalaya <mohammed.alalaya@gmail.com>
2021-06-22 02:15:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+let gifts = []
+
+function  writeCards(gifts)  {
+  let i = 0; // the initialization moves OUTSIDE the body of the loop!
+  while (i < gifts.length) {
+    gifts[i] = `[ 'Thank you, ${gift}, for the wonderful surprise gift!`
+    i++; // the iteration moves INSIDE the body of the loop!
+  }
+  return gifts;
+}
